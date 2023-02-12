178aab031df2afd76929439c912eb393c45b8b45
alisahib909 <alisahibshirmammadli909@gmail.com>
2023-02-13 03:48:33 +0400

    fix network problem

--- cache/redisClient.go
+++ cache/redisClient.go
@@ -49,7 +49,7 @@ func NewRedisClientForDev() *redis.Client {
 	fmt.Print(addrs)
 
 	opts := &redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "redis:6379",
 		Password: "",
 		DB:       0,
 	}
