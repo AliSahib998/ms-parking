ea6b999ff214bec0a9649609321a5ca98a95836f
alisahib909 <alisahibshirmammadli909@gmail.com>
2023-02-13 03:01:44 +0400

    add random parking slots

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ import (
 	"github.com/AliSahib998/ms-parking/model"
 	"github.com/AliSahib998/ms-parking/service"
 	"github.com/go-chi/chi"
+	"github.com/go-redis/redis/v8"
 	log "github.com/sirupsen/logrus"
+	"math/rand"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 // @title Api Documentation
@@ -25,37 +28,9 @@ func main() {
 	config.LoadConfig()
 	router := chi.NewRouter()
 	redisClient := cache.NewRedisClientForDev()
-	var slot = []model.Slot{
-		{
-			SlotNumber: 267829,
-			IsEmpty:    true,
-			SlotType:   model.TwoWheeler,
-			Vehicle:    nil,
-		},
 
-		{
-			SlotNumber: 22222,
-			IsEmpty:    true,
-			SlotType:   model.TwoWheeler,
-			Vehicle:    nil,
-		},
-
-		{
-			SlotNumber: 222221,
-			IsEmpty:    true,
-			SlotType:   model.TwoWheeler,
-			Vehicle:    nil,
-		},
-
-		{
-			SlotNumber: 2224221,
-			IsEmpty:    true,
-			SlotType:   model.TwoWheeler,
-			Vehicle:    nil,
-		},
-	}
-	r, _ := json.Marshal(slot)
-	redisClient.Set(context.TODO(), model.SLOTS, r, 0)
+	//for adding random parking slots
+	loadParkingSlots(redisClient)
 
 	var redisHelper = &service.RedisHelper{
 		RedisClient: redisClient,
@@ -68,4 +43,25 @@ func main() {
 	port := strconv.Itoa(config.Props.Port)
 	log.Info("Starting server at port: ", port)
 	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+func loadParkingSlots(client *redis.Client) {
+	var slots []model.Slot
+	for i := 0; i < 100; i++ {
+		var slotType = model.FourWheeler
+		if i%3 == 0 {
+			slotType = model.TwoWheeler
+		}
+		var slot = model.Slot{
+			SlotNumber: rand.New(
+				rand.NewSource(time.Now().UnixNano())).Int(),
+			IsEmpty:  false,
+			SlotType: slotType,
+			Vehicle:  nil,
+		}
+		slots = append(slots, slot)
+	}
+
+	r, _ := json.Marshal(slots)
+	client.Set(context.TODO(), model.SLOTS, r, 0)
 }
